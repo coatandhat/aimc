42534467c101c6f88d0a8ab8d3b38262f0edce27
tomwalters <tomwalters@47b1ee86-127b-13bb-b609-7b595106ed26>
2010-02-24 14:49:35 +0000

    -Script updates for EC2

--- scripts/ec2_user_data.sh
+++ scripts/ec2_user_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -O- run.alestic.com/install/runurl | bash
-runurl run.alestic.com/email/start youremail@example.com
+runurl run.alestic.com/email/start tom@tomwalters.co.uk
 runurl http://aimc.googlecode.com/svn/trunk/scripts/aws_prepare.sh
