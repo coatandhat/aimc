5eea223f0985a1b12f42522223eb59e02ae00bf0
tomwalters <tomwalters@47b1ee86-127b-13bb-b609-7b595106ed26>
2010-08-10 12:51:31 +0000

    - Added automation for AWS

--- experiments/scripts/setup_aws_instance.sh
+++ experiments/scripts/setup_aws_instance.sh
@@ -2,3 +2,10 @@
 # Run ami-2fc2e95b (32 bit) or ami-05c2e971 (64 bit) in eu-west zone 
 sudo apt-get -y update
 sudo apt-get -y install subversion scons pkg-config libsndfile-dev build-essential libboost-dev libc6-dev-i386 python
+
+sudo mkdir /mnt/aimc
+sudo chown ubuntu /mnt/aimc
+cd /mnt/aimc
+svn checkout http://aimc.googlecode.com/svn/trunk/ aimc-read-only
+cd aimc-read-only/experiments
+./master.sh
