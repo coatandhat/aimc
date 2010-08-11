7201e9b878a02ecb25f350c5c42b42b95a56a2e0
tomwalters <tomwalters@47b1ee86-127b-13bb-b609-7b595106ed26>
2010-08-11 11:59:20 +0000

    - AWS (bis)

--- trunk/experiments/scripts/aimc/build_aimc.sh
+++ trunk/experiments/scripts/aimc/build_aimc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Set up AIM-C on an Amazon EC2 instance. A useful reference for building.
+set -e
+set -u
+
 AIMC_DIR=/mnt/experiments/aimc
 
 sudo mkdir -p $AIMC_DIR
@@ -10,3 +13,5 @@ cd aimc-read-only
 scons
 cd ..
 export PATH=$PATH:$AIMC_DIR/aimc-read-only/build/posix-release/
+touch $AIMC_DIR/.aimc_build_success
+cd -
