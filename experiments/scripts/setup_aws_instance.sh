19aa16b2abc4cd033a896230b5e40af8a5decb81
tomwalters <tomwalters@47b1ee86-127b-13bb-b609-7b595106ed26>
2010-08-11 08:55:29 +0000

    - AWS fixes (part 1)

--- experiments/scripts/setup_aws_instance.sh
+++ experiments/scripts/setup_aws_instance.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # Run ami-2fc2e95b (32 bit) or ami-05c2e971 (64 bit) in eu-west zone 
+# ec2-run-instances  --user-data-file ec2_user_data.sh \
+#                    --key tom_eu_west \ 
+#                    --instance-type m1.small  \
+#                    --instance-count 1   \
+#                    --region eu-west-1   \
+#                    --availability-zone eu-west-1b \
+#                    ami-2fc2e95b
+
 sudo apt-get -y update
 sudo apt-get -y install subversion scons pkg-config libsndfile-dev build-essential libboost-dev libc6-dev-i386 python
 
