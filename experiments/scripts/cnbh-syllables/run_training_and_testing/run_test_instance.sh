f556b78442ebd81dd874c806e3da0f9a30abbd62
tomwalters <tomwalters@47b1ee86-127b-13bb-b609-7b595106ed26>
2010-08-12 14:21:02 +0000

    - AWs

--- experiments/scripts/cnbh-syllables/run_training_and_testing/run_test_instance.sh
+++ experiments/scripts/cnbh-syllables/run_training_and_testing/run_test_instance.sh
@@ -25,6 +25,9 @@ TEST_SCRIPT=testing_script
 SYLLIST_COMPLETE=syllable_list_with_silence
 TRAIN_MLF=training_master_label_file
 
+DICT=dictionary
+WDNET=word_network
+
 SILENCE=sil
 
 THIS_DIR=`dirname $0`
