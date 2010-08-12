920ba384514c727c8d3cafa1368fce77d75058fd
tomwalters <tomwalters@47b1ee86-127b-13bb-b609-7b595106ed26>
2010-08-12 15:44:10 +0000

    - AWS - enable logging

--- trunk/experiments/scripts/setup_aws_instance.sh
+++ trunk/experiments/scripts/setup_aws_instance.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # Run ami-2fc2e95b (32 bit) or ami-05c2e971 (64 bit) in eu-west zone 
-# ec2-run-instances  --user-data-file ec2_user_data.sh \
-#                    --key tom_eu_west \ 
-#                    --instance-type m1.small  \
-#                    --instance-count 1   \
-#                    --region eu-west-1   \
-#                    --availability-zone eu-west-1b \
-#                    ami-2fc2e95b
+# ec2-run-instances  --user-data-file ec2_user_data.sh --key tom_eu_west --instance-type m1.small --instance-count 1 --region eu-west-1 --availability-zone eu-west-1b ami-2fc2e95b
 
 sudo apt-get -y update
 sudo apt-get -y install bc subversion scons pkg-config libsndfile1-dev build-essential libboost-dev python sox
@@ -16,7 +10,9 @@ sudo apt-get -y install bc subversion scons pkg-config libsndfile1-dev build-ess
 
 sudo mkdir -p /mnt/aimc
 sudo chown ubuntu /mnt/aimc
+sudo mkdir -p /mnt/log
+sudo chown ubuntu /mnt/log
 cd /mnt/aimc
 svn checkout http://aimc.googlecode.com/svn/trunk/ aimc-read-only
 cd aimc-read-only/experiments/scripts/
-./master.sh
+./master.sh > /mnt/log/log.log
