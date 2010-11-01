69b46a0cb4bc126f5f61a562cc748b02519ac94f
tomwalters <tomwalters@47b1ee86-127b-13bb-b609-7b595106ed26>
2010-11-01 01:29:33 +0000

    - Updated results plotting

--- trunk/experiments/scripts/cnbh-syllables/results_plotting/plot_munged_results.py
+++ trunk/experiments/scripts/cnbh-syllables/results_plotting/plot_munged_results.py
@@ -16,11 +16,14 @@ for line in f:
     results.setdefault(values[3],dict())
     results[values[3]].setdefault(values[0], dict())
     results[values[3]][values[0]].setdefault(values[1], dict())
+    results[values[3]][values[0]][values[1]].setdefault(int(values[4]), dict())
+    results[values[3]][values[0]][values[1]][int(values[4])].setdefault(int(values[5]), dict())
+    results[values[3]][values[0]][values[1]][int(values[4])][int(values[5])].setdefault(int(values[6]), dict())
     if values[2] == 'clean':
-       snr = 40
+       snr = 50
     else:
        snr = int(values[2])
-       results[values[3]][values[0]][values[1]][snr] = float(values[4])
+       results[values[3]][values[0]][values[1]][int(values[4])][int(values[5])][int(vlues[6])][snr] = float(values[7])
 #    results[values[3]].append((values[1],values[2],values[2],values[4]))
 
 ax = plt.subplot(111)
@@ -28,9 +31,12 @@ ax = plt.subplot(111)
 train_set = 'inner'
 lines = []
 labels = []
+hmm_iterations = 2
+hmm_states = 4
+hmm_components = 4
 for feature_type in ('mfcc', 'mfcc_vtln', 'aim'):
   for feature_subtype in results[train_set][feature_type].keys():
-    this_line = results[train_set][feature_type][feature_subtype].items()
+    this_line = results[train_set][feature_type][feature_subtype][hmm_states][hmm_components][hmm_iterations].items()
     this_line.sort(cmp=lambda x,y: x[0] - y[0])
     xs, ys = zip(*this_line)
     xs = list(xs)
