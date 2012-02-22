8b981a021f07cdb143f1b94907def2130524c139
tom@acousticscale.org <tom@acousticscale.org@47b1ee86-127b-13bb-b609-7b595106ed26>
2012-02-22 14:09:47 +0000

    Final automation steps for starting the server.

--- trunk/scripts/aim-o-matic/prepare_aws_instance_stage_2.sh
+++ trunk/scripts/aim-o-matic/prepare_aws_instance_stage_2.sh
@@ -5,4 +5,6 @@ sudo apache2ctl restart
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../../
 scons
+sudo cp aimc/scripts/aim-o-matic/aim-o-matic.php aimc/scripts/aim-o-matic/upload-form.html /var/www
+echo "*/10 * * * * /home/ubuntu/aimc/scripts/aim-o-matic/aim_o_matic_cronjob.sh" | crontab
 
