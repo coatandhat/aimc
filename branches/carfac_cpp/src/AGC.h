ea720712d6e8814b7cf7f24b2617a6c7d2420c63
Ulf.Hammarqvist@gmail.com <Ulf.Hammarqvist@gmail.com@47b1ee86-127b-13bb-b609-7b595106ed26>
2012-03-28 16:44:46 +0000

    stubilub lolipop

--- branches/carfac_cpp/src/AGC.h
+++ branches/carfac_cpp/src/AGC.h
@@ -1,10 +1,22 @@
 #ifndef AGC_H_
 #define AGC_H_
 
+#include "CARFAC_common_typedefs.h"
+
 class AGC_parameters {
 public:
 	AGC_parameters();
 	virtual ~AGC_parameters();
+
+	float n_stages;
+	FloatArray time_constants;
+	float AGC_stage_gain;
+	FloatArray decimation;
+	FloatArray AGC1_scales;
+	FloatArray AGC2_scales;
+	float detect_scale;
+	float AGC_mix_coeff;
+
 };
 
 class AGC_coefficients {
