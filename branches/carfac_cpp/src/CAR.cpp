76925d4de966ca8a9366627fa48bbc2fb97b24e5
Ulf.Hammarqvist@gmail.com <Ulf.Hammarqvist@gmail.com@47b1ee86-127b-13bb-b609-7b595106ed26>
2012-03-31 18:15:17 +0000

    stubbed CAR_coefficients constructor

--- branches/carfac_cpp/src/CAR.cpp
+++ branches/carfac_cpp/src/CAR.cpp
@@ -1,9 +1,27 @@
 #include "CAR.h"
 #include "CARFAC_common.h"
+#include <cmath>
 
-CAR_coefficients::CAR_coefficients(CAR_parameters* CAR_params, float fs,
+CAR_coefficients::CAR_coefficients(CAR_parameters* car_params, float fs,
                                    FloatArray pole_freqs){
-  // TODO stuff goes here
+
+  // initialisation of the FloatArray coefficients, if needed.
+
+  float f = pow(car_params->zero_ratio_, 2) + 1;
+
+  FloatArray theta = pole_freqs;
+  FloatArray c0 = pole_freqs;
+  FloatArray a0 = pole_freqs;
+
+  // temp until we get eigen in
+  for(float i=0; i<theta.size(); i++){
+      theta[i] *= (2*kPi*fs); // scalar mult.
+      c0[i] = sin(theta[i]);
+      a0[i] = cos(theta[i]);
+  }
+
+  // etc
+
 }
 
 CAR_coefficients::~CAR_coefficients(){
