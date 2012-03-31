56cee077be3a7ddc9ec5f2c951b6c1a444a7c64d
Ulf.Hammarqvist@gmail.com <Ulf.Hammarqvist@gmail.com@47b1ee86-127b-13bb-b609-7b595106ed26>
2012-03-31 17:24:08 +0000

    stubbed some more, and tidying

--- branches/carfac_cpp/src/CARFAC.cpp
+++ branches/carfac_cpp/src/CARFAC.cpp
@@ -3,27 +3,42 @@
 #include "IHC.h"
 #include "AGC.h"
 
-#include "CARFAC_common_typedefs.h"
+#include "CARFAC_common.h"
 
 CARFAC::CARFAC(int fs = kDefaultFs,
               CAR_parameters* car_params = new CAR_parameters(),
               IHC_parameters* ihc_params = new IHC_parameters(),
               AGC_parameters* agc_params = new AGC_parameters(),
               float erb_break_freq = kDefaultErbBreakFreq,
-              float erb_q = kDefaultErbQ){
+              float erb_q = kDefaultErbQ) : n_ears_(0) {
 
-  // for now, design is to take ownership. Preferences? Make copies, call by value, etc?
+  // Design is to take ownership. Preferences? Make copies, call by value, etc?
   car_params_ = car_params;
   ihc_params_ = ihc_params;
   agc_params_ = agc_params;
 
-  FloatArray pole_freqs; //TODO: do it
-  car_coeffs_ = new CAR_coefficients(car_params_, fs, pole_freqs);
+  float pole_hz = car_params->first_pole_theta_* fs / (2*kPi);
+  while (pole_hz > car_params->min_pole_hz_){
+      pole_freqs_.push_back(pole_hz); // STL specific
+      pole_hz = pole_hz - car_params->erb_per_step_ *
+                ERB_Hz(pole_hz, erb_break_freq, erb_q);
+  }
+  n_ch_ = pole_freqs_.size(); // STL specific
 
-  int n_ch = 10; // TODO: do the design loops
-  agc_coeffs_ = new AGC_coefficients(agc_params_, fs, n_ch);
+  // replace with feeding this (as const ref) instead? Saves storing doubly
+  car_coeffs_ = new CAR_coefficients(car_params_, fs_, pole_freqs_);
+  ihc_coeffs_ = new IHC_coefficients(ihc_params_, fs_, n_ch_);
+  agc_coeffs_ = new AGC_coefficients(agc_params_, fs_, n_ch_);
 
-  ihc_coeffs_ = new IHC_coefficients(ihc_params_, fs, n_ch);
+  // move this into AGC_coefficients constructor instead
+  agc_coeffs_->detect_scale_ = agc_params_->detect_scale_ /
+                               (ihc_coeffs_->saturation_output_ *
+                               agc_coeffs_->agc_gain_);
+}
+
+//move this somewhere else?
+float CARFAC::ERB_Hz(float cf_hz, float erb_break_freq, float erb_q){
+  return (erb_break_freq + cf_hz) / erb_q;
 }
 
 CARFAC::~CARFAC() {
--- branches/carfac_cpp/src/IHC.h
+++ branches/carfac_cpp/src/IHC.h
@@ -2,18 +2,20 @@
 #define IHC_H_
 
 // not sure how to best deal with the "three style" IHC - ulha
-class IHC_parameters {
+class IHC_parameters{
 public:
-  IHC_parameters()
-  {
-    tau_lpf_ = 0.000080;
-    tau1_out_ = 0.020;
-    tau1_in_ = 0.020;
-    tau2_out_ = 0.005;
-    tau2_in_ = 0.005;
+  IHC_parameters() :
+    tau_lpf_(0.000080),
+    tau1_out_(0.020),
+    tau1_in_(0.20),
+    tau2_out_(0.005),
+    tau2_in_(0.005){
+    // do nothing more
   }
 
-  virtual ~IHC_parameters(){}
+  virtual ~IHC_parameters(){
+    // do nothing
+  }
 
   float tau_lpf_;
   float tau1_out_;
@@ -22,12 +24,15 @@ public:
   float tau2_in_;
 };
 
-class IHC_coefficients {
+class IHC_coefficients{
 public:
   IHC_coefficients(IHC_parameters*, float, int);
   virtual ~IHC_coefficients();
+
+  float saturation_output_;
+
 private:
-  IHC_coefficients(){}
+  IHC_coefficients();
 };
 
 #endif /* IHC_H_ */
